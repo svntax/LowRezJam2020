a69d29a0a383cda29cb185573c5dadd588a798ef
svntax <endless.answers@gmail.com>
2020-07-31 22:19:04 -0400

    Basic setup

---
+++ Main.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	# Default size 512x512
+	OS.window_size = Vector2(512, 512)
+	# Center the window after resizing
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
